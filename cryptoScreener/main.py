a6031e719bdb196e9f037ec24dc6f856cc0861fd
antho <anthonyjrchen@gmail.com>
2023-10-21 01:33:00 -0700

    first commit

---
+++ cryptoScreener/main.py
@@ -0,0 +1,185 @@
+from typing import List
+from cryptoScreener import models
+from fastapi import FastAPI, Request, UploadFile, File, Depends, HTTPException,status
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import JSONResponse, RedirectResponse
+from fastapi_jwt_auth import AuthJWT
+from fastapi_jwt_auth.exceptions import AuthJWTException
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from cryptoScreener.database import SessionLocal, engine
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import NoResultFound
+from cryptoScreener.models import Data, Notification, Status, Token, Request_Status, Proof, Commitment
+from pydantic import BaseModel
+import pandas as pd
+import os
+import time
+from datetime import timedelta
+from passlib.context import CryptContext
+import random
+import uuid
+
+class UserRequest(BaseModel):
+    username: str
+    password: str
+
+class DataRequest(BaseModel):
+    item: str
+
+class UsernameRequest(BaseModel):
+    username: str
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="cryptoScreener/static"), name="static")
+templates = Jinja2Templates(directory = "cryptoScreener/templates")
+models.Base.metadata.create_all(bind=engine)
+IMAGEDIR = os.path.join(os.path.dirname(__file__), "uploads/")
+
+SECRET_KEY = "a969379d6774b629d92dc6118ccb4c35f2676e5c3152bff4c9fbfb1bbff1bc0c"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+class Settings(BaseModel):
+    authjwt_secret_key: str = SECRET_KEY
+    authjwt_token_location: set = {"cookies"}
+    authjwt_cookie_csrf_protect: bool = False
+    authjwt_cookie_samesite: str = 'lax'
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+@AuthJWT.load_config
+def get_config():
+    return Settings()
+
+@app.exception_handler(AuthJWTException)
+def authjwt_exception_handler(request: Request, exc: AuthJWTException):
+    if exc.message=="Signature has expired":
+        return RedirectResponse("/")
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message}
+    )
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(username: str, db):
+    users = db.query(models.User).filter(models.User.username==username).first()
+    return users
+    
+def authenticate_user(username: str, password: str, db):
+    user = get_user(username, db)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+# security end
+
+@app.post("/login")
+async def login_for_access_token(from_data: OAuth2PasswordRequestForm = Depends(), Authorize: AuthJWT = Depends(), db : Session = Depends(get_db)):
+    user = authenticate_user(from_data.username, from_data.password, db)
+    print("got some user")
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Incorrect username or password",
+                            headers={"WWW-Authenticate": "Bearer"})
+    print("user is true")
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = Authorize.create_access_token(subject=user.username, algorithm=ALGORITHM ,expires_time=access_token_expires)
+    refresh_token = Authorize.create_refresh_token(subject=user.username, algorithm=ALGORITHM ,expires_time=access_token_expires)
+    Authorize.set_access_cookies(access_token)
+    Authorize.set_refresh_cookies(refresh_token)
+    return {"msg":"Login Successful"}
+
+@app.delete('/logout')
+def logout(Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
+    Authorize.unset_jwt_cookies()
+    return {"msg": "Logout Successful"}
+
+@app.get('/retrieve/user')
+def get_username(Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
+    return Authorize.get_jwt_subject()
+
+@app.get("/")
+def dashboard(request: Request):
+    return templates.TemplateResponse("landing_page.html", {
+        "request":request,
+    })
+
+@app.get("/signup")
+def dashboard(request: Request):
+    return templates.TemplateResponse("signup.html", {
+        "request":request,
+    })
+
+@app.post("/signup")
+async def verifyUser(user_request: UserRequest, db : Session = Depends(get_db)):
+    try:
+        print("entering signup func")
+        hashed_password = get_password_hash(user_request.password)
+        priv_key_list = db.query(models.User.private_key).all()
+        print(priv_key_list)
+        random_key = random.choice(list(set([x for x in range(0,100)]) - set(priv_key_list)))
+        # generate user index_set
+        user_index_set = random.sample(range(0,127),64)
+        user = models.User(username=user_request.username, hashed_password=hashed_password, disabled=True, private_key=random_key, index_set=str(user_index_set))
+        db.add(user)
+        db.commit()
+        return {"True"}
+    except Exception as err:
+        print(err)
+        return {"False"}
+
+
+@app.get("/dashboard")
+async def dashboard(request: Request, db : Session = Depends(get_db), Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
+    username = Authorize.get_jwt_subject()
+    datas = db.query(Data).filter(Data.author!=username)
+    
+    token_list = db.query(models.Token).filter(models.Token.buyer_id==username).all()
+    request_status_list = db.query(Request_Status.data_name,Request_Status.request_status).filter(Request_Status.requestor==username).all()
+    status_dict = {}
+    requested_data = []
+    for status in request_status_list:
+        status_dict[status.data_name]=status.request_status
+        requested_data.append(status.data_name)
+    return templates.TemplateResponse("dashboard.html", {
+        "request":request,
+        "datas":datas,
+        "status_dict":status_dict,
+        "token_list":token_list,
+        "requested_data":requested_data,
+        "page_location":"Dash",
+    })
+
+@app.get("/user/priv_key")
+def get_priv_key(db : Session = Depends(get_db), Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
+    username = Authorize.get_jwt_subject()
+    retrieved_key = db.query(models.User).filter(models.User.username==username).first()
+    try:
+        return retrieved_key.private_key
+    except AttributeError as err:
+        raise err
+
+@app.get("/contacts")
+def contacts(request:Request):
+    return templates.TemplateResponse("contact.html", {
+        "request":request,
+    })
---
+++ cryptoScreener/models.py
@@ -0,0 +1,85 @@
+from sqlalchemy import Column, Integer, String, Date, Boolean, Identity, BLOB, Numeric
+from sqlalchemy.orm import relationship, Mapped
+from typing import List
+from cryptoScreener.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    username = Column(String, unique=True, index = True)
+    user_id = Column(Integer, primary_key = True, index = True)
+    hashed_password = Column(String)
+    disabled = Column(Boolean, default=True)
+    private_key = Column(Integer)
+    index_set = Column(String)
+    
+
+class Data(Base):
+    __tablename__ = "datas"
+    id = Column(String, primary_key=True)
+    data_cred = Column(String)
+    data_name = Column(String)
+    date_added = Column(String)
+    author = Column(String)
+    author_id = Column(Integer, index=True)
+
+class Request_Status(Base):
+    __tablename__ = "request_status_list"
+    id = Column(Integer, primary_key = True, index=True)
+    data_name = Column(String)
+    requestor = Column(String)
+    requestee = Column(String)
+    request_status = Column(String)
+    request_timestamp = Column(String)
+
+class Status(Base):
+    __tablename__ = "status_list"
+    id = Column(Integer, primary_key=True)
+    data_name = Column(String)
+    data_author = Column(String)
+    data_receiver = Column(String)
+    approval_status = Column(Boolean, default=False)
+
+class Notification(Base):
+    __tablename__ = "notifications"
+    id = Column(Integer, primary_key=True, index=True)
+    requestee = Column(String)
+    requestor = Column(String)
+    data_name = Column(String)
+    message = Column(String)
+
+class Commitment(Base):
+    __tablename__ = "commitment_list"
+    commitment_id = Column(Integer, primary_key=True, index=True)
+    time_stamp = Column(String)
+    user_id = Column(Integer, index=True)
+    commitment = Column(String)
+    rx_value = Column(String)
+    data_id = Column(String)
+    approved_ids = Column(BLOB)
+
+class Proof(Base):
+    __tablename__ = "proof_list"
+    proof_id = Column(Integer, primary_key=True, index=True)
+    time_stamp = Column(String)
+    user_id = Column(Integer)
+    proof = Column(String)
+    data_id = Column(String)
+    approved_ids = Column(BLOB)
+
+class Token(Base):
+    __tablename__ = "token_list"
+    token_id = Column(Integer, primary_key=True, index=True)
+    time_stamp = Column(String)
+    commitment_received = Column(String)
+    buyer_id = Column(Integer)
+    data_id = Column(String)
+    approved_ids = Column(BLOB)
+
+class Crypto(Base):
+    __tablename__ = "cryptos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    symbol = Column(String, unique=True, index = True)
+    price = Column(Numeric(10,5))
+    ma50 = Column(Numeric(10,5))
+    ma200 = Column(Numeric(10,5))
